6bbbebcfb2a1445f43aa5acc7d7f721fc7803b5f
unknown <peng42236@gmail.com>
2026-02-05 08:57:27 +0800

    Jerico

--- src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-function Header() {
-  return (
-    <div className="text-5xl bg-blue-600 text-white font-bold text-center py-6 shadow-md">
-      Jerico works
-    </div>
-  );
-}
-
-export default Header;
