e4ff6bdfe328c80a5dec2797fba43c573c721f3a
unknown <peng42236@gmail.com>
2026-02-12 15:49:44 +0800

    Jerico

--- client/client/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-function Header() {
-  return (
-    <div className="w-full max-w-md text-center py-4 bg-white rounded-lg shadow">
-      <h1 className="text-xl font-bold">To-Do-List</h1>
-    </div>
-  );
-}
-
-export default Header;
